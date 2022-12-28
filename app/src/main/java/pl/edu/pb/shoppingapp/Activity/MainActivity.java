e18c7e194083d5e8cf43b0755251138c01d61e25
mpietruczuk07 <mateusz_pietruczuk07@wp.pl>
2022-12-28 17:42:15 +0100

    Added view for fragment "more".

--- app/src/main/java/pl/edu/pb/shoppingapp/Activity/MainActivity.java
+++ app/src/main/java/pl/edu/pb/shoppingapp/Activity/MainActivity.java
@@ -64,114 +64,6 @@ public class MainActivity extends AppCompatActivity {
             }
             return true;
         });
-
-        RecyclerView recyclerView = findViewById(R.id.recyclerview);
-
-        final ProductAdapter adapter = new ProductAdapter();
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-        productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
-        productViewModel.findAll().observe(this, adapter::setProducts);
-
-        FloatingActionButton addBookButton = findViewById(R.id.add_button);
-        addBookButton.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, EditProductActivity.class);
-            startActivityForResult(intent, NEW_PRODUCT_ACTIVITY_REQUEST_CODE);
-        });
-
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == NEW_PRODUCT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
-            Product product = new Product(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE), 1, "TEST", false);
-            productViewModel.insert(product);
-            //snackbar!!!
-        }
-
-        if (requestCode == EDIT_PRODUCT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
-            editedProduct.setTitle(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE));
-//            editedProduct.setNote(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE));
-//            editedProduct.setNote(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE));
-            productViewModel.update(editedProduct);
-            editedProduct = null;
-            //snackbar!!!
-        }
-    }
-
-    private class ProductHolder extends RecyclerView.ViewHolder {
-        private TextView productTitleTextView;
-        private TextView productQuantityTextView;
-        private Product product;
-
-        public ProductHolder(LayoutInflater inflater, ViewGroup parent) {
-            super(inflater.inflate(R.layout.product_item_list, parent, false));
-
-            productTitleTextView = itemView.findViewById((R.id.product_title));
-            productQuantityTextView = itemView.findViewById((R.id.product_quantity));
-
-            View productItem = itemView.findViewById(R.id.product_item);
-
-            productItem.setOnLongClickListener(v -> {
-                productViewModel.delete(product);
-                System.out.println("Skasowano");
-                Snackbar.make(findViewById(R.id.main_view), getString(R.string.book_deleted), Snackbar.LENGTH_LONG).show();
-                return true;
-            });
-
-            productItem.setOnClickListener(v -> {
-                editedProduct = product;
-                Intent intent = new Intent(MainActivity.this, EditProductActivity.class);
-                intent.putExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE, productTitleTextView.getText());
-                //intent.putExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE,  productNoteTextView.getText());
-                startActivityForResult(intent, EDIT_PRODUCT_ACTIVITY_REQUEST_CODE);
-            });
-
-        }
-
-        public void bind(Product product) {
-            productTitleTextView.setText(product.getTitle());
-            productQuantityTextView.setText(Integer.toString(product.getQuantity()));
-            this.product = product;
-        }
-    }
-
-    private class ProductAdapter extends RecyclerView.Adapter<ProductHolder> {
-        private List<Product> products;
-
-        @NonNull
-        @Override
-        public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return new ProductHolder(getLayoutInflater(), parent);
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
-            if (products != null) {
-                Product book = products.get(position);
-                holder.bind(book);
-            } else {
-                System.out.println("brak");
-                Log.d("MainActivity", "No products");
-            }
-        }
-
-        @Override
-        public int getItemCount() {
-            if (products != null) {
-                return products.size();
-            } else {
-                return 0;
-            }
-        }
-
-        void setProducts(List<Product> products) {
-            this.products = products;
-            notifyDataSetChanged();
-        }
     }
 
     private void replaceFragment(Fragment fragment){
@@ -180,4 +72,113 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.main_view, fragment);
         fragmentTransaction.commit();
     }
-}
+
+//        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+//
+//        final ProductAdapter adapter = new ProductAdapter();
+//        recyclerView.setAdapter(adapter);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+//
+//        productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
+//        productViewModel.findAll().observe(this, adapter::setProducts);
+//
+//        FloatingActionButton addBookButton = findViewById(R.id.add_button);
+//        addBookButton.setOnClickListener(v -> {
+//            Intent intent = new Intent(MainActivity.this, EditProductActivity.class);
+//            startActivityForResult(intent, NEW_PRODUCT_ACTIVITY_REQUEST_CODE);
+//        });
+//
+//    }
+
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == NEW_PRODUCT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+//            Product product = new Product(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE), 1, "TEST", false);
+//            productViewModel.insert(product);
+//            //snackbar!!!
+//        }
+//
+//        if (requestCode == EDIT_PRODUCT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+//            editedProduct.setTitle(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE));
+////            editedProduct.setNote(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE));
+////            editedProduct.setNote(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE));
+//            productViewModel.update(editedProduct);
+//            editedProduct = null;
+//            //snackbar!!!
+//        }
+//    }
+//
+//    private class ProductHolder extends RecyclerView.ViewHolder {
+//        private TextView productTitleTextView;
+//        private TextView productQuantityTextView;
+//        private Product product;
+//
+//        public ProductHolder(LayoutInflater inflater, ViewGroup parent) {
+//            super(inflater.inflate(R.layout.product_item_list, parent, false));
+//
+//            productTitleTextView = itemView.findViewById((R.id.product_title));
+//            productQuantityTextView = itemView.findViewById((R.id.product_quantity));
+//
+//            View productItem = itemView.findViewById(R.id.product_item);
+//
+//            productItem.setOnLongClickListener(v -> {
+//                productViewModel.delete(product);
+//                System.out.println("Skasowano");
+//                Snackbar.make(findViewById(R.id.main_view), getString(R.string.book_deleted), Snackbar.LENGTH_LONG).show();
+//                return true;
+//            });
+//
+//            productItem.setOnClickListener(v -> {
+//                editedProduct = product;
+//                Intent intent = new Intent(MainActivity.this, EditProductActivity.class);
+//                intent.putExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE, productTitleTextView.getText());
+//                //intent.putExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE,  productNoteTextView.getText());
+//                startActivityForResult(intent, EDIT_PRODUCT_ACTIVITY_REQUEST_CODE);
+//            });
+//
+//        }
+//
+//        public void bind(Product product) {
+//            productTitleTextView.setText(product.getTitle());
+//            productQuantityTextView.setText(Integer.toString(product.getQuantity()));
+//            this.product = product;
+//        }
+//    }
+//
+//    private class ProductAdapter extends RecyclerView.Adapter<ProductHolder> {
+//        private List<Product> products;
+//
+//        @NonNull
+//        @Override
+//        public ProductHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//            return new ProductHolder(getLayoutInflater(), parent);
+//        }
+//
+//        @Override
+//        public void onBindViewHolder(@NonNull ProductHolder holder, int position) {
+//            if (products != null) {
+//                Product book = products.get(position);
+//                holder.bind(book);
+//            } else {
+//                System.out.println("brak");
+//                Log.d("MainActivity", "No products");
+//            }
+//        }
+//
+//        @Override
+//        public int getItemCount() {
+//            if (products != null) {
+//                return products.size();
+//            } else {
+//                return 0;
+//            }
+//        }
+//
+//        void setProducts(List<Product> products) {
+//            this.products = products;
+//            notifyDataSetChanged();
+//        }
+//    }
+    }
