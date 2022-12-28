761050dc0e61cc9c68f28a309e5d8dce4d711652
mpietruczuk07 <mateusz_pietruczuk07@wp.pl>
2022-12-28 15:33:30 +0100

    Bottom menu and fragments creation.

--- app/src/main/java/pl/edu/pb/shoppingapp/Activity/MainActivity.java
+++ app/src/main/java/pl/edu/pb/shoppingapp/Activity/MainActivity.java
@@ -3,6 +3,9 @@ package pl.edu.pb.shoppingapp.Activity;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,23 +22,51 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import java.util.List;
+import java.util.Map;
 
+import pl.edu.pb.shoppingapp.Fragment.FavouriteShopsFragment;
+import pl.edu.pb.shoppingapp.Fragment.HomeFragment;
+import pl.edu.pb.shoppingapp.Fragment.MapsFragment;
+import pl.edu.pb.shoppingapp.Fragment.MoreFragment;
 import pl.edu.pb.shoppingapp.Product;
 import pl.edu.pb.shoppingapp.ProductViewModel;
 import pl.edu.pb.shoppingapp.R;
+import pl.edu.pb.shoppingapp.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
     private ProductViewModel productViewModel;
     private Product editedProduct;
+    private ActivityMainBinding binding;
+
     public static final int NEW_PRODUCT_ACTIVITY_REQUEST_CODE = 1;
     public static final int EDIT_PRODUCT_ACTIVITY_REQUEST_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.bottomNavMenu.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.home_btn:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.maps_btn:
+                    replaceFragment(new MapsFragment());
+                    break;
+                case R.id.favourite_shops_btn:
+                    replaceFragment(new FavouriteShopsFragment());
+                    break;
+                case R.id.more_btn:
+                    replaceFragment(new MoreFragment());
+                    break;
+            }
+            return true;
+        });
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
         final ProductAdapter adapter = new ProductAdapter();
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -55,7 +86,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == NEW_PRODUCT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK){
+        if (requestCode == NEW_PRODUCT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
             Product product = new Product(data.getStringExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE), 1, "TEST", false);
             productViewModel.insert(product);
             //snackbar!!!
@@ -91,9 +122,9 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             });
 
-            productItem.setOnClickListener(v->{
+            productItem.setOnClickListener(v -> {
                 editedProduct = product;
-                Intent intent  = new Intent(MainActivity.this, EditProductActivity.class);
+                Intent intent = new Intent(MainActivity.this, EditProductActivity.class);
                 intent.putExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_TITLE, productTitleTextView.getText());
                 //intent.putExtra(EditProductActivity.EXTRA_EDIT_PRODUCT_NOTE,  productNoteTextView.getText());
                 startActivityForResult(intent, EDIT_PRODUCT_ACTIVITY_REQUEST_CODE);
@@ -141,5 +172,12 @@ public class MainActivity extends AppCompatActivity {
             this.products = products;
             notifyDataSetChanged();
         }
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.main_view, fragment);
+        fragmentTransaction.commit();
     }
 }
