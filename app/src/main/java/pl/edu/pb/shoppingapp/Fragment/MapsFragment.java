dd524fd431411ba05a8d80777f2b32342175d81d
mpietruczuk07 <mateusz_pietruczuk07@wp.pl>
2022-12-30 17:57:22 +0100

    Fixing a location sensor dialog and adding a location permission request.

--- app/src/main/java/pl/edu/pb/shoppingapp/Fragment/MapsFragment.java
+++ app/src/main/java/pl/edu/pb/shoppingapp/Fragment/MapsFragment.java
@@ -4,16 +4,17 @@ import static android.app.Activity.RESULT_CANCELED;
 import static android.app.Activity.RESULT_OK;
 
 import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
-import android.location.LocationRequest;
 import android.os.Bundle;
 
-import androidx.activity.result.IntentSenderRequest;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -23,32 +24,40 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.Priority;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.maps.errors.ApiException;
+import com.google.android.material.snackbar.Snackbar;
 
 import pl.edu.pb.shoppingapp.R;
 import pl.edu.pb.shoppingapp.databinding.FragmentMapsBinding;
 
-public class MapsFragment extends Fragment {
+public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private FragmentMapsBinding binding;
     private GoogleMap mGoogleMap;
-    private Boolean isLocationPermission;
+    private Boolean isLocationPermission = true;
     private LocationRequest locationRequest;
-    private LocationCallback callback;
+    private LocationCallback locationCallback;
+
+    private static final int LOCATION_REQUEST_CODE = 1000;
+    private static final int LOCATION_SENSOR = 2000;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         binding = FragmentMapsBinding.inflate(inflater, container, false);
+
+        binding.currentLocationBtn.setOnClickListener(v -> getLocationUpdates());
+
         return binding.getRoot();
     }
 
@@ -56,76 +65,116 @@ public class MapsFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        if (isLocationPermission) {
-            SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager()
-                    .findFragmentById(R.id.fragment_map);
-        }
+        SupportMapFragment supportMapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.fragment_map);
+        supportMapFragment.getMapAsync(this);
+
     }
 
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
         mGoogleMap = googleMap;
-        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        if (ActivityCompat
-                .checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat
-                .checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            return;
+
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            isLocationPermission = true;
+            if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                    && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                isLocationPermission = false;
+                return;
+            }
+
+            mGoogleMap.setMyLocationEnabled(true);
+            mGoogleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mGoogleMap.getUiSettings().setTiltGesturesEnabled(true);
+
+            getLocationUpdates();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+            new AlertDialog.Builder(requireContext())
+                    .setTitle(getString(R.string.location_permission_title))
+                    .setMessage(getString(R.string.app_name) + " " + getString(R.string.location_permission_message))
+                    .setPositiveButton(getString(R.string.positive_button_ok), new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION}, LOCATION_REQUEST_CODE);
+                        }
+                    }).create()
+                    .show();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_BACKGROUND_LOCATION}, LOCATION_REQUEST_CODE);
         }
-        googleMap.setMyLocationEnabled(true);
-        googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
+
+        mGoogleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
-                checkGPS();
+                getLocationUpdates();
                 return false;
             }
         });
     }
 
-    public void checkGPS() {
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    isLocationPermission = true;
+                    getLocationUpdates();
+                }
+                else{
+                    Snackbar.make(requireActivity().findViewById(R.id.fragment_map), getText(R.string.location_permission_denied), Snackbar.LENGTH_LONG).show();
+                }
+        }
+    }
+
+    public void getLocationUpdates() {
         locationRequest = LocationRequest.create();
         locationRequest.setInterval(10000);
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequest)
                 .setAlwaysShow(true);
+
         Task<LocationSettingsResponse> locationSettingsResponseTask = LocationServices
-                .getSettingsClient(getApplicationContext())
+                .getSettingsClient(getContext())
                 .checkLocationSettings(builder.build());
+
         locationSettingsResponseTask.addOnCompleteListener(new OnCompleteListener<LocationSettingsResponse>() {
-               @Override
-               public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
-                   try {
-                       LocationSettingsResponse response = task.getResult(ApiException.class);
-                   }
-                   catch (ApiException e) {
-                       if(e.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
-                           ResolvableApiException resolvableApiException = (ResolvableApiException) e;
-                           try {
-                               resolvableApiException.startResolutionForResult(getActivity(), 101);
-                           }
-                           catch (IntentSender.SendIntentException sendIntentException) {
-                               sendIntentException.printStackTrace();
-                           }
-                       }
-                       if(e.getStatusCode() == LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE) {
-                           Toast.makeText(getActivity(), "Unavaliable", Toast.LENGTH_SHORT).show();
-                       }
-                   }
-               }
-           });
+            @Override
+            public void onComplete(@NonNull Task<LocationSettingsResponse> task) {
+                try {
+                    LocationSettingsResponse response = task.getResult(com.google.android.gms.common.api.ApiException.class);
+                } catch (com.google.android.gms.common.api.ApiException e) {
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.RESOLUTION_REQUIRED) {
+                        ResolvableApiException resolvableApiException = (ResolvableApiException) e;
+                        try {
+                            resolvableApiException.startResolutionForResult(getActivity(), LOCATION_SENSOR);
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                    }
+                    if (e.getStatusCode() == LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE) {
+                        Toast.makeText(getActivity(), getString(R.string.location_change_unavailable), Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
     }
 
+    //doesn't work - never shows up
     @Override
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == 101) {
-            if(resultCode == RESULT_OK) {
-                Toast.makeText(getContext(), "Enabled", Toast.LENGTH_SHORT).show();
-            }
-            if(resultCode == RESULT_CANCELED) {
-                Toast.makeText(getContext(), "Denied", Toast.LENGTH_SHORT).show();
+        switch (requestCode){
+            case LOCATION_SENSOR:
+                if(resultCode == RESULT_OK){
+                    Toast.makeText(getContext(), getString(R.string.location_sensor_enabled), Toast.LENGTH_SHORT).show();
+                }
+                if(resultCode == RESULT_CANCELED){
+                    Toast.makeText(getContext(), getString(R.string.location_sensor_disabled), Toast.LENGTH_LONG).show();
             }
         }
     }
