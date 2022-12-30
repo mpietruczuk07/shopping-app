935a829b35df5689827964d76930d92026c60be7
mpietruczuk07 <mateusz_pietruczuk07@wp.pl>
2022-12-30 20:26:16 +0100

    Showing the current location on the Google Maps.

--- app/src/main/java/pl/edu/pb/shoppingapp/Fragment/MapsFragment.java
+++ app/src/main/java/pl/edu/pb/shoppingapp/Fragment/MapsFragment.java
@@ -9,6 +9,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,23 +19,34 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResponse;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.location.Priority;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -43,12 +56,19 @@ import pl.edu.pb.shoppingapp.databinding.FragmentMapsBinding;
 public class MapsFragment extends Fragment implements OnMapReadyCallback {
     private FragmentMapsBinding binding;
     private GoogleMap mGoogleMap;
-    private Boolean isLocationPermission = true;
+    private Boolean isLocationPermission = false;
+    private Location currentLocation;
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
+    private LocationManager locationManager;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private Marker currentMarker;
 
     private static final int LOCATION_REQUEST_CODE = 1000;
     private static final int LOCATION_SENSOR = 2000;
+    private static final float ZOOM = 17;
+
+    private static final String TAG = "MAPS_FRAGMENT";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -120,8 +140,7 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     isLocationPermission = true;
                     getLocationUpdates();
-                }
-                else{
+                } else {
                     Snackbar.make(requireActivity().findViewById(R.id.fragment_map), getText(R.string.location_permission_denied), Snackbar.LENGTH_LONG).show();
                 }
         }
@@ -132,6 +151,8 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
         locationRequest.setInterval(10000);
         locationRequest.setFastestInterval(5000);
         locationRequest.setPriority(Priority.PRIORITY_HIGH_ACCURACY);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
 
         LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
                 .addLocationRequest(locationRequest)
@@ -161,6 +182,102 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
                 }
             }
         });
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                if (locationResult != null) {
+                    for (Location location : locationResult.getLocations()) {
+                        Log.d(TAG, "onLocationResult: " + " " + location.getLatitude() + " " + location.getLongitude());
+                    }
+                }
+            }
+        };
+
+        startLocationUpdates();
+    }
+
+    private void startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            isLocationPermission = false;
+            return;
+        }
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Starting location updates.");
+                }
+            }
+        });
+
+        getCurrentLocation();
+    }
+
+    private void getCurrentLocation() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(requireContext());
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            isLocationPermission = false;
+            return;
+        }
+
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                currentLocation = location;
+                moveToLocation(currentLocation);
+            }
+        });
+    }
+
+
+    //change name to current user!
+    private void moveToLocation(Location location) {
+        if (location != null) {
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), ZOOM);
+            MarkerOptions markerOptions = new MarkerOptions().position(new LatLng(location.getLatitude(), location.getLongitude())).title(getString(R.string.current_location))
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE)).snippet("User");
+
+            if (currentMarker != null) {
+                currentMarker.remove();
+            }
+
+            currentMarker = mGoogleMap.addMarker(markerOptions);
+            currentMarker.setTag(703);
+            mGoogleMap.animateCamera(cameraUpdate);
+        }
+    }
+
+    private void stopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+        Log.d(TAG, "Stopping location updates.");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        if (fusedLocationProviderClient != null) {
+            stopLocationUpdates();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        if (fusedLocationProviderClient != null) {
+            startLocationUpdates();
+        }
+
+        if (currentMarker != null) {
+            currentMarker.remove();
+        }
     }
 
     //doesn't work - never shows up
@@ -168,14 +285,14 @@ public class MapsFragment extends Fragment implements OnMapReadyCallback {
     public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        switch (requestCode){
+        switch (requestCode) {
             case LOCATION_SENSOR:
-                if(resultCode == RESULT_OK){
+                if (resultCode == RESULT_OK) {
                     Toast.makeText(getContext(), getString(R.string.location_sensor_enabled), Toast.LENGTH_SHORT).show();
                 }
-                if(resultCode == RESULT_CANCELED){
+                if (resultCode == RESULT_CANCELED) {
                     Toast.makeText(getContext(), getString(R.string.location_sensor_disabled), Toast.LENGTH_LONG).show();
-            }
+                }
         }
     }
 }
